8955450304d68ae680f0da69f86c65996a74a826
Lymar Volodymyr <shaxtar11@gmail.com>
2024-11-18 17:19:49 +0100

    Refactored Announcement; Added model AnnouncementTranslation to create separate translations for 1 announcement via a ForeignKey.

--- infrastructure/models/announcement_model.py
+++ infrastructure/models/announcement_model.py
@@ -1,8 +1,8 @@
 from datetime import datetime
 from typing import Optional
 
-from sqlalchemy import BigInteger, DateTime, Integer, String
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import BigInteger, DateTime, ForeignKey, Integer, String, UniqueConstraint
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from infrastructure.models.base import Base
 
@@ -12,9 +12,27 @@ class Announcement(Base):
 
     id = mapped_column(Integer, primary_key=True)
     title: Mapped[str] = mapped_column(String(50), nullable=False, index=True)
-    text: Mapped[str] = mapped_column(String(3500), nullable=False)
     image_url: Mapped[Optional[str]] = mapped_column(String, nullable=True)
     created_at: Mapped[datetime] = mapped_column(
         DateTime, default=lambda: datetime.now().replace(second=0, microsecond=0)
     )
     created_by: Mapped[int] = mapped_column(BigInteger, nullable=False)
+    translations_text = relationship(
+        'AnnouncementTranslation',
+        back_populates='announcement',
+        cascade='all, delete-orphan'
+    )
+
+
+class AnnouncementTranslation(Base):
+    __tablename__ = 'announcement_translations'
+    __table_args__ = (
+        UniqueConstraint('announcement_id', 'language_code', name='uc_announcement_language'),
+    )
+
+    id = mapped_column(Integer, primary_key=True)
+    announcement_id = mapped_column(Integer, ForeignKey('announcements.id'), nullable=False)
+    language_code = mapped_column(String(10), nullable=False)
+    text: Mapped[str] = mapped_column(String(3500), nullable=False)
+
+    announcement = relationship('Announcement', back_populates='translations_text')
