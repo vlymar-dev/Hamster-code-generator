aad74243549f61fedf649ce640b9e57c9d3bb8a0
Lymar Volodymyr <shaxtar11@gmail.com>
2024-11-24 21:34:36 +0100

    Create GameTask model and repository, added methods.

---
+++ infrastructure/models/game_task.py
@@ -0,0 +1,13 @@
+from sqlalchemy import Integer, String, Text
+from sqlalchemy.orm import Mapped, mapped_column
+
+from infrastructure.models.base import Base
+
+
+class GameTask(Base):
+    __tablename__ = 'game_tasks'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
+    game_name: Mapped[str] = mapped_column(String(50), nullable=False, index=True)
+    task: Mapped[str] = mapped_column(Text, nullable=False)
+    answer: Mapped[str] = mapped_column(Text, nullable=False)
