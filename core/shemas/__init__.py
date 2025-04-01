7c2bd8cfd63e6dfb9c027f38ca6e64011027b7e2
Lymar Volodymyr <shaxtar11@gmail.com>
2025-04-01 23:01:07 +0200

    refactor: made fixes to user_progress_handler, created ProgresText to generate text, created schemas to validate progress data

---
+++ core/shemas/__init__.py
@@ -0,0 +1,3 @@
+from core.shemas.user import UserCreateSchema, UserProgresSchema, UserProgressData
+
+__all__ = ['UserCreateSchema', 'UserProgresSchema', 'UserProgressData']
--- core/shemas/user.py
+++ core/shemas/user.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from pydantic import BaseModel
 
 
@@ -7,3 +9,20 @@ class UserCreateSchema(BaseModel):
     last_name: str | None = None
     username: str | None = None
     language_code: str
+
+
+class UserProgresSchema(BaseModel):
+    registration_date: datetime
+    user_status: str
+    total_keys_generated: int
+
+class UserProgressData(BaseModel):
+    total_keys: int
+    user_status: str
+    days_in_bot: int
+    referrals: int
+    current_level: str
+    next_level: str | None
+    keys_progress: str
+    referrals_progress: str
+    days_progress: str
